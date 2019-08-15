aaf270835a23ca0f42d7c1fcc628afbc23ce2c54
simplecoder25 <oshustarsen4@gmail.com>
2019-08-16 00:15:36 +0300

    Add something new

--- src/main/java/controls/ActionsWithJSON.java
+++ src/main/java/controls/ActionsWithJSON.java
@@ -1,15 +1,22 @@
 package controls;
 
+import com.google.gson.Gson;
 import io.restassured.response.Response;
 import methodsAPI.PetServices;
 import models.Pet;
 
-public class FormatFromJson {
+public class ActionsWithJSON {
     private Response response;
 
     public Pet fromJson(int id) {
         response = new PetServices().getPetByID(id);
         Pet newPet = response.as(Pet.class);
         return newPet;
+    }
+
+    public static String formatToJSon(Pet pet) {
+        Gson gson = new Gson();
+        String toJson = gson.toJson(pet);
+        return toJson;
     }
 }
